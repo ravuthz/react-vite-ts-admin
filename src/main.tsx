0a28c2b72a1057d07d9f3a99e365c0cb3bc0d73a
ravuthz <ravuthz@gmail.com>
2022-08-19 14:03:59 +0700

    WIP: Add project to git

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// import 'antd/dist/antd.css';
+// import 'react-antd-ts/node_modules/antd/dist/antd.css';
+// import 'react-antd-ts/dist/layouts/AdminLayout/AdminLayout.css';
+
+import  { AdminLayout }  from 'react-antd-ts';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AdminLayout>
+      <App></App>
+    </AdminLayout>
   </React.StrictMode>
 )
